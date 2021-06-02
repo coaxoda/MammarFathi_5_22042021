b1d853f7e7d21437d15bbd5210546b05eb5913a9
Mammar fathi <mammar.fathi@gmail.com>
2021-06-02 11:25:12 +0200

    MàJ req ajax pour faire afficher la page produit, a présent seul le produit correspondant est récuprer

--- website/js/article.js
+++ website/js/article.js
@@ -1,9 +1,11 @@
 async function getArticle() {
+    const urlId = new URLSearchParams(window.location.search)
+    const cameraId = urlId.get('id');
     try{
-        let res = await fetch("http://localhost:3000/api/cameras/")
+        let res = await fetch("http://localhost:3000/api/cameras/" + cameraId)
         if (res.ok){
             let cameras = await res.json()
-            articleGenerator._generateArticle(cameras)  // calling static function _generateArticle(cameras) of the class "articleGenerator" in "/articleGenerator.js"
+            articleGenerator._generateArticle(cameras, cameraId)  // calling static function _generateArticle(cameras) of the class "articleGenerator" in "/articleGenerator.js"
         }else {
             console.error('Error !')
         }
--- website/js/classes/articleGenerator.js
+++ website/js/classes/articleGenerator.js
@@ -1,53 +1,50 @@
 class articleGenerator {
-    
-    static _generateArticle(cameras) {
-            // Creating of the main section/ base of implementation for "articleItem.js"
+
+    static _generateArticle(cameras, cameraId) {
+        // Creating of the main section/ base of implementation for "articleItem.js"
         let articleSection = document.createElement('section');
         let mainContent = document.querySelector('main');
         mainContent.appendChild(articleSection);
+        //Get product ID by location to the next page + finding all ID corresponding to the product selected previously
 
-            //Get product ID by location to the next page + finding all ID corresponding to the product selected previously
-        const urlId = new URLSearchParams(window.location.search)
-        const cameraId = urlId.get('id');
         console.log(cameraId);
-        const camProduct = cameras.find(cameras => cameras['_id'] == cameraId);
-        console.log(camProduct);
+        // const cameraId = cameras.find(cameras => cameras['_id'] == cameraId);
 
-            //New instance of the class article "articleItem.js" with params -- (cameras, articleSection, camProduct) -- previously created in "/article.js" and here
-        new article(cameras, articleSection, camProduct)
+        //New instance of the class article "articleItem.js" with params -- (cameras, articleSection, cameraId) -- previously created in "/article.js" and here
+        new article(cameras, articleSection, cameraId)
     }
-} 
+}
 class article {
-    
-    constructor(cameras, articleSection, camProduct) {
+
+    constructor(cameras, articleSection) {
         let articleItem = this._article(articleSection)
-        this._articleImg(camProduct, articleItem)
+        this._articleImg(cameras, articleItem)
         let articleBody = this._articleContent(articleSection)
-        this._articleDescription(articleBody, camProduct)
-        this._articleTitle(articleBody, camProduct)
-        this._articlePrice(camProduct, articleBody)
-        this._articleBtn(articleSection, camProduct)
-        this.camProduct = camProduct
+        this._articleDescription(articleBody, cameras)
+        this._articleTitle(articleBody, cameras)
+        this._articlePrice(cameras, articleBody)
+        this._articleBtn(articleSection, cameras)
+        this.cameraId = cameraId
         this.cameras = cameras
-        this._articleLens(articleBody, camProduct)
+        this._articleLens(articleBody, cameras)
     }
 
     //Creating of an article parent
-    _article(articleSection){
+    _article(articleSection) {
         const mainArticle = document.createElement('article')
         mainArticle.id = "productID"
         articleSection.appendChild(mainArticle)
         return mainArticle
     }
     //Generating an img (article child) depending of the url id
-    _articleImg(camProduct, articleItem){
+    _articleImg(cameras, articleItem) {
         const articleImg = document.createElement('img')
         articleImg.className = "productID-item"
-        articleImg.src = camProduct.imageUrl
+        articleImg.src = cameras.imageUrl
         articleItem.appendChild(articleImg)
     }
     //Creating parent Div for title, price...
-    _articleContent(articleSection){
+    _articleContent(articleSection) {
         const articleContent = document.createElement('div')
         articleContent.className = "product-content"
         articleSection.appendChild(articleContent)
@@ -55,67 +52,67 @@ class article {
     }
 
     //Article description for
-    _articleDescription(articleBody, camProduct){
+    _articleDescription(articleBody, cameras) {
         const articleDes = document.createElement('p')
-        articleDes.innerHTML = camProduct.description
+        articleDes.innerHTML = cameras.description
         articleBody.appendChild(articleDes)
     }
     //Article title
-    _articleTitle(articleBody, camProduct){
+    _articleTitle(articleBody, cameras) {
         const articleTitle = document.createElement('h1')
-        articleTitle.innerHTML = camProduct.name
+        articleTitle.innerHTML = cameras.name
         articleBody.appendChild(articleTitle)
     }
     //Article price
-    _articlePrice(camProduct, articleBody){
+    _articlePrice(cameraId, articleBody) {
         const articlePrice = document.createElement('p')
-        articlePrice.innerHTML = camProduct.price / 100 + "€"
+        articlePrice.innerHTML = cameraId.price / 100 + "€"
         articleBody.appendChild(articlePrice)
-    } 
+    }
     //Article Lenses options
-    _articleLens(articleBody, camProduct){
+    _articleLens(articleBody, cameras) {
         let articleLensSect = document.createElement("p");
         articleLensSect.classList.add('lense');
         articleLensSect.innerText = "Objectif :";
         articleBody.appendChild(articleLensSect);
         //Creating of a select element with different lens Value
         let selectLens = document.createElement("select");
-        let lense = camProduct.lenses;
+        let lense = cameras.lenses;
         articleBody.appendChild(selectLens);
         // console.log(lense)
-        //Display all lense available for each camProduct item
+        //Display all lense available for each cameraId item
         for (let i = 0; i < lense.length; i++) {
             let lensesOption = document.createElement("option");
-            lensesOption.textContent = camProduct.lenses[i];
+            lensesOption.textContent = cameras.lenses[i];
             selectLens.appendChild(lensesOption);
         }
     }
     //Add article into chekout
-    _articleBtn(articleSection, camProduct){
+    _articleBtn(articleSection, cameras) {
         const articleBtn = document.createElement('button')
         articleBtn.className = "add"
         articleBtn.type = "submit"
         articleBtn.innerHTML = "Ajouter au panier"
         articleSection.appendChild(articleBtn)
-        articleBtn.addEventListener('click', function(){
+        articleBtn.addEventListener('click', function () {
             //Creation of an empty array, ready for receive item to checkout
             let myCart = JSON.parse(localStorage.getItem("myCart"))
-                if(myCart === null){
-                    myCart = []
-                }
+            if (myCart === null) {
+                myCart = []
+            }
             let myArticle = {
-                name : camProduct.name,
-                price : camProduct.price,
-                img : camProduct.imageUrl,
-                id: camProduct._id
+                name: cameras.name,
+                price: cameras.price,
+                img: cameras.imageUrl,
+                id: cameras._id
             }
             // item pushed into "myCart" array
-            myCart.push(myArticle) 
+            myCart.push(myArticle)
             localStorage.setItem("myCart", JSON.stringify(myCart))
             console.log(myCart);
-            
+
             let notification = document.getElementById('succes')
-            notification.style.display ="flex"
+            notification.style.display = "flex"
         })
     }
 }
